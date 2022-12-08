db47916f1302b4327ae8c9987ea8c1f97ceffc5b
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-12-08 02:36:53 +0100

    added and fixed bugs for advanced tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
+
 def weight_average(my_list=[]):
-    """
-    Returns the weighted average of all integers tuple (<score>, <weight>).
-
-    Args:
-        my_list: Given list
-
-    Return:
-        The weighted average
-    """
-    total_score, total_weight = 0, 0
+    total_score = 0
+    total_weight = 0
 
     if len(my_list) == 0:
         return 0
