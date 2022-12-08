a4c0d4c579a919d18fe4f78058bac67da8c35aec
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-12-08 02:56:57 +0100

    fixed bugs and changed file permissions

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    for key in a_dictionary:
+    dict_list = list(a_dictionary.keys())
+
+    for key in dict_list:
         if value == a_dictionary.get(key):
             del a_dictionary[key]
     return a_dictionary
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@
 def print_sorted_dictionary(a_dictionary):
     dict_list = sorted(a_dictionary.items())
     for item in dict_list:
-        print(item[0], item[1])
+        print(f"{item[0]}: {item[1]}")
