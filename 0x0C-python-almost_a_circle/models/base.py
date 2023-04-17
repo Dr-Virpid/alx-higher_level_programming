7c0e3a278e8ee29aa72b20694de88ceb4d6a0de3
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2023-04-17 22:09:36 +0100

    Create base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Base class module
+"""
+
+
+class Base:
+    """
+    A Base Class with autoincrementing id
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize class Base
+
+        Args:
+            id: id of Base instance
+        """
+        if id is not None:
+            self.id = id
+        else:
+            type(Base.__nb_objects) += 1
+            self.id = Base.__nb_objects
